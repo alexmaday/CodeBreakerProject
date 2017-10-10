9e8118082108bf9c77cc33ec256386105f5728ed
Alex Maday <alex.maday@gmail.com>
2017-10-10 15:53:36 -0500

    from guess, call setHiddenFields *only* when answer & attempt are empty

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,11 +4,16 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+
+    // only set hidden fields if they are empty.
+    if (!answer.value && !attempt.value) {
+        setHiddenFields();
+    }
 }
 
 //implement new functions here
 function setHiddenFields() {
-    answer = Math.floor(Math.random() * 10000).toString();
+    answer.value = Math.floor(Math.random() * 10000).toString();
 
     // make sure answer is always 4 'digits' long, even padded with zeros
     // if necessary
@@ -16,5 +21,5 @@ function setHiddenFields() {
         answer = '0' + answer;
     }
 
-    attempt.innerHTML = 0;
+    attempt.value = 0;
 }
